bfa2b1ac9ff90149dcd0e922794b5b397d0301c1
PanJinhao <2158870977@qq.com>
2024-05-06 15:49:41 +0800

    编写结构框架

--- app/blog/internal/data/tag.go
+++ app/blog/internal/data/tag.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-kratos/kratos/v2/log"
 	api "my-kratos-blog/api/tag"
+	"my-kratos-blog/app/blog/internal/biz"
 )
 
 type tagRepo struct {
@@ -11,6 +12,12 @@ type tagRepo struct {
 	log  *log.Helper
 }
 
+func NewTagRepo(data *Data) biz.TagRepo {
+	return &tagRepo{
+		data: data,
+		log:  log.NewHelper(log.With()),
+	}
+}
 func (r *tagRepo) CreateTag(ctx context.Context, request *api.CreateTagRequest) *api.CreateTagReply {}
 
 func (r *tagRepo) DeleteTag(ctx context.Context, request *api.DeleteTagRequest) *api.DeleteTagReply {}
